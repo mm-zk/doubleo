78afabe200e0f378cba7b9ba867e9c073febe328
mm <mm@matterlabs.dev>
2024-08-02 11:51:19 +0200

    trying out rpsee

--- src/proxy.rs
+++ src/proxy.rs
@@ -5,7 +5,9 @@ use jsonrpc_core::{BoxFuture, Result};
 use std::{future::Future, pin::Pin, str::FromStr};
 use tokio::runtime::Builder;
 use zksync_types::{
-    api::{BlockIdVariant, BlockNumber, Transaction, TransactionReceipt, TransactionVariant},
+    api::{
+        BlockId, BlockIdVariant, BlockNumber, Transaction, TransactionReceipt, TransactionVariant,
+    },
     transaction_request::CallRequest,
     url::SensitiveUrl,
     web3::{Bytes, FeeHistory, Index, SyncState},
@@ -13,7 +15,8 @@ use zksync_types::{
 };
 use zksync_web3_decl::{
     client::{Client, L2},
-    namespaces::EthNamespaceClient,
+    jsonrpsee::core::{async_trait, RpcResult},
+    namespaces::{EthNamespaceClient, EthNamespaceServer},
     types::{Block, Filter, FilterChanges, Log},
 };
 
@@ -60,6 +63,694 @@ where
     })
     .join()
     .unwrap()
+}
+
+#[async_trait]
+impl EthNamespaceServer for Proxy {
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_block_number<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U64>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn chain_id<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U64>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn call<'life0, 'async_trait>(
+        &'life0 self,
+        req: CallRequest,
+        block: Option<BlockIdVariant>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Bytes>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn estimate_gas<'life0, 'async_trait>(
+        &'life0 self,
+        req: CallRequest,
+        _block: Option<BlockNumber>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn gas_price<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn new_filter<'life0, 'async_trait>(
+        &'life0 self,
+        filter: Filter,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn new_block_filter<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn uninstall_filter<'life0, 'async_trait>(
+        &'life0 self,
+        idx: U256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<bool>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn new_pending_transaction_filter<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_logs<'life0, 'async_trait>(
+        &'life0 self,
+        filter: Filter,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Vec<Log>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_filter_logs<'life0, 'async_trait>(
+        &'life0 self,
+        filter_index: U256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<FilterChanges>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_filter_changes<'life0, 'async_trait>(
+        &'life0 self,
+        filter_index: U256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<FilterChanges>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_balance<'life0, 'async_trait>(
+        &'life0 self,
+        address: Address,
+        block: Option<BlockIdVariant>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_block_by_number<'life0, 'async_trait>(
+        &'life0 self,
+        block_number: BlockNumber,
+        full_transactions: bool,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<Block<TransactionVariant>>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_block_by_hash<'life0, 'async_trait>(
+        &'life0 self,
+        hash: H256,
+        full_transactions: bool,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<Block<TransactionVariant>>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_block_transaction_count_by_number<'life0, 'async_trait>(
+        &'life0 self,
+        block_number: BlockNumber,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<U256>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_block_receipts<'life0, 'async_trait>(
+        &'life0 self,
+        block_id: BlockId,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<Vec<TransactionReceipt>>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_block_transaction_count_by_hash<'life0, 'async_trait>(
+        &'life0 self,
+        block_hash: H256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<U256>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_code<'life0, 'async_trait>(
+        &'life0 self,
+        address: Address,
+        block: Option<BlockIdVariant>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Bytes>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_storage_at<'life0, 'async_trait>(
+        &'life0 self,
+        address: Address,
+        idx: U256,
+        block: Option<BlockIdVariant>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<H256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_transaction_count<'life0, 'async_trait>(
+        &'life0 self,
+        address: Address,
+        block: Option<BlockIdVariant>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_transaction_by_hash<'life0, 'async_trait>(
+        &'life0 self,
+        hash: H256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<Transaction>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_transaction_by_block_hash_and_index<'life0, 'async_trait>(
+        &'life0 self,
+        block_hash: H256,
+        index: Index,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<Transaction>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_transaction_by_block_number_and_index<'life0, 'async_trait>(
+        &'life0 self,
+        block_number: BlockNumber,
+        index: Index,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<Transaction>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_transaction_receipt<'life0, 'async_trait>(
+        &'life0 self,
+        hash: H256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<TransactionReceipt>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn protocol_version<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<String>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn send_raw_transaction<'life0, 'async_trait>(
+        &'life0 self,
+        tx_bytes: Bytes,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<H256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn syncing<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<SyncState>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn accounts<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Vec<Address>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn coinbase<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Address>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn compilers<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Vec<String>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn hashrate<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<U256>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_uncle_count_by_block_hash<'life0, 'async_trait>(
+        &'life0 self,
+        hash: H256,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<U256>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn get_uncle_count_by_block_number<'life0, 'async_trait>(
+        &'life0 self,
+        number: BlockNumber,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<Option<U256>>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn mining<'life0, 'async_trait>(
+        &'life0 self,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<bool>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
+
+    #[must_use]
+    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
+    fn fee_history<'life0, 'async_trait>(
+        &'life0 self,
+        block_count: U64,
+        newest_block: BlockNumber,
+        reward_percentiles: Vec<f32>,
+    ) -> ::core::pin::Pin<
+        Box<
+            dyn ::core::future::Future<Output = RpcResult<FeeHistory>>
+                + ::core::marker::Send
+                + 'async_trait,
+        >,
+    >
+    where
+        'life0: 'async_trait,
+        Self: 'async_trait,
+    {
+        todo!()
+    }
 }
 
 impl EthNamespaceT for Proxy {
